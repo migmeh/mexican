06f4b4bacf6301c5002ee70fe468e82369e8e3ab
migmeh <xmigmeh@gmail.com>
2020-11-17 00:48:09 -0600

    cambio uno

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!doctype html>
 <html lang="es">
 <head>
-    
-    
-    
+
+
+
 <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
 <script type="text/javascript" src="js/my.js"></script>
-    
+
 <link href="images/icono.ico" rel="shortcut icon" type="image/x-icon">
 <link href="estilo.css" rel="stylesheet" type="text/css">
-    
-    
-    
-    
+
+
+
+
 <meta charset="utf-8">
 
-<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=0,maximum-scale=1,user-scalable=no">    
-    
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=0,maximum-scale=1,user-scalable=no">
+
 
 
 <meta name="robots" content="all" />
@@ -31,24 +31,24 @@
   <ul class="menu">
    <li  class="inicio"><a href="#inicio">Inicio</a></li>
   <li  class="about"><a href="#about">About</a></li>
-    
+
 </ul>
-  
-  
 
 
-      
+
+
+
     <div id="contenedor" >
     <div id="uno" >
     	<img src="images/trajinera.jpg" alt="xochimilco" class="imagen" />
    	</div>
     </div>
-  
+
    <div id="contenedor_dos">
-   
+
     <div id="dos" >
-    <h1>15 de septiembre</h1>
-    <h2> día de la Independencia de México</h2>
+    <h1>El día 15 de septiembre</h1>
+    <h2>Pagina del día de la Independencia de México</h2>
     <br />
     	<p>Todos los años el 15 de septiembre en México se festeja el dia de la independencia como parte de nuestras tradiciones, realicé este micrositio como parte de la celebración.</p><br />
         <p>El 16 de septiembre es el Día de la Independencia en México desde que ésta se proclamó. El término popular español para esta celebración es el Grito de Dolores</p><br />
@@ -56,9 +56,9 @@
 <p>Esto se lo debemos a Porfirio Díaz, el famoso presidente que gobernó nuestro país durante casi 30 años. Sucede que don Porfirio festejaba su cumpleaños el día 15 de septiembre (efectivamente día de los Porfirios). Sin embargo, y para empatar su onomástico con los festejos del Grito (en ocasión del centenario de la Independencia), decidió adelantar éste por unas horas. Así pues, desde 1910, el Grito de Independencia como festejo popular, tiene lugar los días 15 de septiembre, cerca de las 11 de la noche y no los 16 por la madrugada como realmente sucedió en 1810.</p><br />
 
    	</div>
-    
+
    </div>
-  
+
   <?php
   include("pie.php");
   ?>
